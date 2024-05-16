543047345025e50adc2eb059b2b26a56dcc7e5d5
Messyas <messyasgoisfranca@gmail.com>
2024-05-16 13:15:37 -0400

    Configuracao de interface e cores

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,23 @@
+import { Box, CssBaseline, ThemeProvider} from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return  <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+      <div className="app">
+        <BrowserRouter>
+         <ThemeProvider theme={theme}>
+          <CssBaseline />
+            <Box width = "100%" height="100%" padding="1rem 2rem">
+
+            </Box>
+         </ThemeProvider>
+        </BrowserRouter>
+      </div>
+  );
 }
 
 export default App;
