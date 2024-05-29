d5a5ae821a2d8813e3537f64af21edd43a10310d
Messyas <messyasgoisfranca@gmail.com>
2024-05-29 14:27:04 -0400

    mongo db conection

--- server/index.js
+++ server/index.js
@@ -6,18 +6,37 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
+
 /* CONFIGURATIONS */
+
+
 dotenv.config()
+
 const app = express()
+
 app.use( express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin:"}))
 app.use(morgar("common"))
 app.use(bodyParter.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
-
-console.log('hello')
+app.use(cors());
 
 /* Mongoose setup*/
-/* banco de dados */
+
+
+const PORT = process.env.PORT || 9000;
+
+    mongoose
+
+
+        .connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        .then(async () => {
+            app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+        })
+
+        .catch((error) => console.log(`${error} did not connect`));
